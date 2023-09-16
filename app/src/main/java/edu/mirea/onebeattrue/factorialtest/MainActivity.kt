eaabedd175075385a01becde38b07069268cfca7
Vitaly <onebeattrue@gmail.com>
2023-09-16 18:17:37 +0300

    Каркас приложения

--- app/src/main/java/edu/mirea/onebeattrue/factorialtest/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/factorialtest/MainActivity.kt
@@ -2,6 +2,9 @@ package edu.mirea.onebeattrue.factorialtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import edu.mirea.onebeattrue.factorialtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +13,42 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        observeViewModel()
+        binding.buttonCalculate.setOnClickListener {
+            viewModel.calculate(binding.editTextNumber.text.toString())
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.progress.observe(this) {
+            if (it) {
+                binding.progressBarLoading.visibility = View.VISIBLE
+                binding.buttonCalculate.isEnabled = false
+            } else {
+                binding.progressBarLoading.visibility = View.GONE
+                binding.buttonCalculate.isEnabled = true
+            }
+        }
+
+        viewModel.error.observe(this) {
+            if (it) {
+                Toast.makeText(
+                    this,
+                    "Вы не ввели значение!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        viewModel.factorial.observe(this) {
+            binding.textViewResult.text = it
+        }
     }
 }
