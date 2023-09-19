16575f627e075afb0e7c861d063f00b7a8da99bc
Vitaly <onebeattrue@gmail.com>
2023-09-20 02:30:59 +0300

    Рефакторинг Создание класса State (взято из udf архитектуры)

--- app/src/main/java/edu/mirea/onebeattrue/factorialtest/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/factorialtest/MainActivity.kt
@@ -27,28 +27,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun observeViewModel() {
-        viewModel.progress.observe(this) {
-            if (it) {
-                binding.progressBarLoading.visibility = View.VISIBLE
-                binding.buttonCalculate.isEnabled = false
-            } else {
-                binding.progressBarLoading.visibility = View.GONE
-                binding.buttonCalculate.isEnabled = true
-            }
-        }
-
-        viewModel.error.observe(this) {
-            if (it) {
+        viewModel.state.observe(this) {
+            if (it.isError) {
                 Toast.makeText(
                     this,
                     "Вы не ввели значение!",
                     Toast.LENGTH_SHORT
                 ).show()
             }
-        }
 
-        viewModel.factorial.observe(this) {
-            binding.textViewResult.text = it
+            if (it.isInProgress) {
+                binding.progressBarLoading.visibility = View.VISIBLE
+                binding.buttonCalculate.isEnabled = false
+            } else {
+                binding.progressBarLoading.visibility = View.GONE
+                binding.buttonCalculate.isEnabled = true
+            }
+
+            binding.textViewResult.text = it.factorial
         }
     }
 }
---
+++ app/src/main/java/edu/mirea/onebeattrue/factorialtest/State.kt
@@ -0,0 +1,7 @@
+package edu.mirea.onebeattrue.factorialtest
+
+class State(
+    val isError: Boolean = false,
+    val isInProgress: Boolean = false,
+    val factorial: String = ""
+)
